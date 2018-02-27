a16b43c6ca4c958e791edf253ca7a2e48d3268c8
Andrey Yarysh <and@yarysh.com>
2018-02-28 00:07:04 +0300

    setup env

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -9,5 +9,5 @@ type App struct {
 }
 
 func (c App) Index() revel.Result {
-	return c.RenderJSON(map[string]string{"app": "car-online_bot"})
+	return c.RenderJSON(map[string]string{"app": revel.AppName})
 }
--- app/init.go
+++ app/init.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"path"
+
 	"github.com/revel/revel"
 )
 
@@ -13,28 +15,23 @@ var (
 )
 
 func init() {
+	revel.ConfPaths = append(
+		revel.ConfPaths,
+		path.Join(revel.BasePath, "conf", "dev"),
+		path.Join(revel.BasePath, "conf", "prod"),
+	)
+
 	// Filters is the default set of global filters.
 	revel.Filters = []revel.Filter{
 		revel.PanicFilter,             // Recover from panics and display an error page instead.
 		revel.RouterFilter,            // Use the routing table to select the right Action
 		revel.FilterConfiguringFilter, // A hook for adding or removing per-Action filters.
 		revel.ParamsFilter,            // Parse parameters into Controller.Params.
-		revel.SessionFilter,           // Restore and write the session cookie.
-		revel.FlashFilter,             // Restore and write the flash cookie.
-		revel.ValidationFilter,        // Restore kept validation errors and save new ones from cookie.
-		revel.I18nFilter,              // Resolve the requested language
 		HeaderFilter,                  // Add some security based headers
 		revel.InterceptorFilter,       // Run interceptors around the action.
 		revel.CompressFilter,          // Compress the result.
 		revel.ActionInvoker,           // Invoke the action.
 	}
-
-	// Register startup functions with OnAppStart
-	// revel.DevMode and revel.RunMode only work inside of OnAppStart. See Example Startup Script
-	// ( order dependent )
-	// revel.OnAppStart(ExampleStartupScript)
-	// revel.OnAppStart(InitDB)
-	// revel.OnAppStart(FillCache)
 }
 
 // HeaderFilter adds common security headers
@@ -48,11 +45,3 @@ var HeaderFilter = func(c *revel.Controller, fc []revel.Filter) {
 
 	fc[0](c, fc[1:]) // Execute the next filter stage.
 }
-
-//func ExampleStartupScript() {
-//	// revel.DevMod and revel.RunMode work here
-//	// Use this script to check for dev mode and set dev/prod startup scripts here!
-//	if revel.DevMode == true {
-//		// Dev mode
-//	}
-//}
