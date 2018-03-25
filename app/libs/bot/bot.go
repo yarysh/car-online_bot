7eccdffcd55760df27a2ad4d6f0336076bf85f08
Andrey Yarysh <and@yarysh.com>
2018-03-25 22:46:38 +0300

    start working

--- app/libs/bot/bot.go
+++ app/libs/bot/bot.go
@@ -21,7 +21,24 @@ func ProcessUpdate(update map[string]interface{}) (map[string]interface{}, error
 
 // Map of command name and func executor
 var commands = map[string]func(payload string, update updateMessage) (map[string]interface{}, error){
+	"start":   start,
 	"set_key": setApiKey,
+}
+
+func start(payload string, update updateMessage) (map[string]interface{}, error) {
+	m := Message{
+		ChatId: update.From.Id,
+		Text: "Этот бот позволяет получить информацию об автомобиле с сайта [car-online.ru](http://car-online.ru/)\n" +
+			"Для того, чтобы начать пользоваться ботом, необходимо ввести ключ для API Car-Online, который вы можете найти в [ЛК](https://panel.car-online.ru)\n" +
+			"Для доступа ко всем функциям бота вы также можете использовать тестовый ключ: *test*.",
+		ParseMode: "Markdown",
+		ReplyMarkup: InlineKeyboard{InlineKeyboard: [][]InlineKeyboardButton{
+			{InlineKeyboardButton{Text: "Установить ключ", CallbackData: "/set_key"}},
+			{InlineKeyboardButton{Text: "Установить тестовый ключ", CallbackData: "/set_key test"}},
+			{InlineKeyboardButton{Text: "Помощь", CallbackData: "/help"}},
+		}},
+	}
+	return m.Send()
 }
 
 // Set car-online api key for user
@@ -39,5 +56,6 @@ func setApiKey(payload string, update updateMessage) (map[string]interface{}, er
 	if err != nil {
 		return nil, errors.New("sql error")
 	}
-	return Message{ChatId: update.From.Id, Text: "API ключ успешно обновлен"}.Send()
+	m := Message{ChatId: update.From.Id, Text: "API ключ успешно обновлен"}
+	return m.Send()
 }
